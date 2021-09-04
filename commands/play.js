e57aa223daea2d92235d4f60c383959858475112
Evan Alioto <EvanJAlioto@gmail.com>
2021-09-03 22:07:01 -0700

    required permissions to use the play command to prevent people in my server from using it to piss me off

--- commands/play.js
+++ commands/play.js
@@ -15,54 +15,67 @@ module.exports = {
     const client = require("..");
     const player = client.player;
     console.log(interaction.guild.id, "player guild");
-    if (!interaction.member.voice.channelId)
-      return await interaction.reply({
-        content: "You are not in a voice channel!",
-        ephemeral: true,
-      });
-    if (
-      interaction.guild.me.voice.channelId &&
-      interaction.member.voice.channelId !==
-        interaction.guild.me.voice.channelId
-    )
-      return await interaction.reply({
-        content: "You are not in my voice channel!",
-        ephemeral: true,
-      });
-    const query = interaction.options.get("query").value;
-    const queue = player.createQueue(interaction.guild, {
-      metadata: {
-        channel: interaction.channel,
-      },
-    });
 
-    // verify vc connection
-    try {
-      if (!queue.connection)
-        await queue.connect(interaction.member.voice.channel);
-    } catch {
-      queue.destroy();
-      return await interaction.deferreply({
-        content: "Could not join your voice channel!",
-        ephemeral: true,
+    if (interaction.user.username === "AlignSD") {
+      if (!interaction.member.voice.channelId)
+        return await interaction.reply({
+          content: "You are not in a voice channel!",
+          ephemeral: true,
+        });
+      if (
+        interaction.guild.me.voice.channelId &&
+        interaction.member.voice.channelId !==
+          interaction.guild.me.voice.channelId
+      )
+        return await interaction.reply({
+          content: "You are not in my voice channel!",
+          ephemeral: true,
+        });
+      const query = interaction.options.get("query").value;
+      const queue = player.createQueue(interaction.guild, {
+        metadata: {
+          channel: interaction.channel,
+        },
       });
-    }
 
-    await interaction.deferReply();
-    const track = await player
-      .search(query, {
-        requestedBy: interaction.user,
-      })
-      .then((x) => x.tracks[0]);
-    if (!track)
+      // verify vc connection
+      try {
+        if (!queue.connection)
+          await queue.connect(interaction.member.voice.channel);
+      } catch {
+        queue.destroy();
+        return await interaction.deferreply({
+          content: "Could not join your voice channel!",
+          ephemeral: true,
+        });
+      }
+
+      if (!interaction.user.username === "AlignSD")
+        return await interaction.reply({
+          content: "Youre not authorized to use that command bitch",
+        });
+
+      console.log("Yes my lord");
+      await interaction.deferReply();
+      const track = await player
+        .search(query, {
+          requestedBy: interaction.user,
+        })
+        .then((x) => x.tracks[0]);
+
+      if (!track)
+        return await interaction.followUp({
+          content: `❌ | Track **${query}** not found!`,
+        });
+
+      queue.play(track);
+
       return await interaction.followUp({
-        content: `❌ | Track **${query}** not found!`,
+        content: `⏱️ | Loading track **${track.title}**!`,
       });
-
-    queue.play(track);
-
-    return await interaction.followUp({
-      content: `⏱️ | Loading track **${track.title}**!`,
-    });
+    } else
+      return await interaction.reply({
+        content: "Youre not authorized to use that command bitch",
+      });
   },
 };
